d2f5485548386f9a88f57fd9229d4c334a936b5b
Tianon Gravi <admwiggin@gmail.com>
2019-06-18 21:29:00 -0700

    Fix "_setup.sh" to always set "$dir" properly

--- _setup.sh
+++ _setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+_dir="$(dirname "$BASH_SOURCE")"
+: "${dir:=$_dir}"
 case "${BASHBREW_NAMESPACE:-}" in
 	'' | tianon)
 		export BASHBREW_LIBRARY="$dir/library" BASHBREW_NAMESPACE='tianon'
